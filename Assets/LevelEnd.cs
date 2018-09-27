e2f922bf7406a28f674bae850cff96400636bd77
Nicolas Paes Leme <nicolaspleme@gmail.com>
2018-09-27 15:20:26 -0300

    Level End + Level 4 + 5 Scene
    Now there's a level end script and object. 
    The level end Object takes an object that contains all of the next level and it's Spawn. When both players are inside the trigger it turns off the old Level and turn on the Object that is on the reference. 
    The script also teleport the players to the spawn point.

---
+++ Assets/LevelEnd.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEnd : MonoBehaviour {
+
+    public GameObject NextLevel;
+    public Transform NextLevelSpawnPoint;
+
+    private List<GameObject> _playersOnTrigger =  new List<GameObject>();
+
+    private void OnEnable()
+    {
+        //_levelEndTrigger = GetComponent<Collider2D>();
+    }
+
+    private void OnPlayersLevelEnd(List<GameObject> _playersOnTrigger)
+    {
+        if(_playersOnTrigger.Count == 2) // If the number of player in the Level End trigger is Equal to 2
+        {
+            transform.parent.gameObject.SetActive(false); // Turn off the whole current Level
+            if (!NextLevel.activeInHierarchy) // If the next level is turned off
+            {
+                NextLevel.SetActive(true); //Turn it on
+                foreach (GameObject go in _playersOnTrigger)
+                {
+                    go.transform.position = NextLevelSpawnPoint.position; // Teleport players to the Spawn point of the NextLevel
+                }
+                _playersOnTrigger.Clear();
+            } 
+
+        }
+    }
+
+    void OnTriggerExit2D()
+    {
+        _playersOnTrigger.Clear();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Other" + other.name + "Tag" + other.tag);
+        if (!_playersOnTrigger.Contains(other.gameObject) && other.tag == "Player")
+        {
+            _playersOnTrigger.Add(other.gameObject);
+            OnPlayersLevelEnd(_playersOnTrigger);
+        }
+    }
+
+}
