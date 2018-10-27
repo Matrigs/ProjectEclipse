78b84a7c6c8a98748194cdb229ce3a4b77cb10a9
Renan da Fonte <renandafonte@hotmail.com>
2018-10-27 14:40:50 -0300

    script to disable trigger in animator

--- Assets/Scripts/PlayerComponent.cs
+++ Assets/Scripts/PlayerComponent.cs
@@ -131,6 +131,7 @@ public class PlayerComponent : MonoBehaviour
 		}
 
 		if(controller.collisionsInf.below && animator.GetCurrentAnimatorStateInfo(0).IsName("JumpDown " + charname)){ 
+			land.Play ();
 			animator.SetTrigger ("Jump end"); 
 			StartCoroutine(JumpDelay("Jump end"));
 		}
@@ -183,7 +184,6 @@ public class PlayerComponent : MonoBehaviour
 		if(velocity.y < 0f)
 		{
 			velocity.y += gravity * fallMultipler * Time.deltaTime;
-			if(!land.isPlaying) land.Play ();
 		}
 		else
 		{			
